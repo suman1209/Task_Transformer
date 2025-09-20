119135d10f8eb47001c0dc3641366309ad54b16d
suman <suman.navaratnarajah@utn.de>
2025-09-21 01:57:01 +0200

    feat(task1): initial backup submission is done, but accuracy is 0.32!

--- task1.py
+++ task1.py
@@ -31,13 +31,26 @@ def get_data_sample(batch_size=1):
     ############################################################################
     # TODO: Calculate the ground truth output for the random sequence and store
     # it in 'gts'.
+    x = random_seq[:, 0]
+    y = random_seq[:, 1]
+    remaining_substring = random_seq[:, 2:]
+    count_x = (remaining_substring == x.unsqueeze(1)).sum(dim=1)
+    count_y = (remaining_substring == y.unsqueeze(1)).sum(dim=1)
+    gts = count_x - count_y
     ############################################################################
 
     # Ensure that GT is non-negative
+    gts += SEQ_LEN  # later during inference, we will revert this by subtracting
     ############################################################################
     # TODO: Why is this needed?
+    """
+    My Answer: 
+    Assuming SEQ_LEN is 5, the output lies in the range [-5, 5], I want to 
+    approach this problem as a multiclass classification and use CrossEntropyLoss,
+    then the target labels must be class indices, so easy way is to add +5 to 
+    the ground truth and turn it into a 11 class classification problem.
+    """
     ############################################################################
-    gts += SEQ_LEN
     return random_seq, gts
 
 # Network definition
@@ -66,8 +79,22 @@ class Net(nn.Module):
         ########################################################################
         # TODO: In the following lines we add a (trainable) positional encoding
         # to our representation. Why is this needed?
+        """
+        My Answer: 
+        This problem does not require positional encoding, because in order to 
+        solve the task, the sequence order does not matter, but the model has
+        to somehow know that the first two elements are special, we cannot do 
+        this using vanilla fixed trignometric position encoding, so its a good
+        idea to make the position encoding trainable, so that the model can 
+        learn the task specific details.
+        """
         # Can you think of another similar task where the positional encoding
         # would not be necessary?
+        """
+        My Answer: 
+        similar tasks where the ordering of input tokens doesnt matter
+        (e.g find the sum, max, min of an input sequence)
+        """
         ########################################################################
         positional_encoding = self.positional_encoding.unsqueeze(0)
         positional_encoding = positional_encoding.repeat([B, 1, 1])
@@ -111,7 +138,20 @@ class EncodingLayer(nn.Module):
 
 ################################################################################
 # TODO: Summarize the idea of attention in a few sentences. What are Q, K and V?
-#
+#        
+"""
+My Answer: 
+Transformers transform the input sequence such that each (transformed) token
+has contextual information about the entire sequence. This is achieved using 
+the attention mechanism, which computes a weighted sum of the values (V), 
+where the weights are determined by the similarity between the queries (Q) 
+and keys (K). Q, K, and V are all derived from the input sequence through 
+learned linear transformations.
+
+Note. if the input sequence is {x1, x2, x3, x4, x5, x6, x7}, then Q contains
+the query vectors {q1, q2, q3, q4, q5, q6, q7} corresponding to each token,
+similarly for K and V
+"""
 # In the following lines, you will implement a naive version of Multi-Head
 # Attention. Please do not derive from the given structure. If you have ideas
 # about how to optimize the implementation you can however note them in a
@@ -124,6 +164,9 @@ class Attention(nn.Module):
 
         ########################################################################
         # TODO: Add necessary member variables.
+        self.W_q = nn.Linear(input_dim, embed_dim)
+        self.W_k = nn.Linear(input_dim, embed_dim)
+        self.W_v = nn.Linear(input_dim, embed_dim)
         ########################################################################
 
     def forward(self, q, k, v):
@@ -133,6 +176,19 @@ class Attention(nn.Module):
         # TODO: First, calculate a trainable linear projection of q, k and v.
         # Then calculate the scaled dot-product attention as described in
         # Section 3.2.1 of the paper.
+        """
+        My Comment: 
+         q, k, v = x, x, x (as used in the EncodingLayer)
+        """
+        Q = self.W_q(q)
+        K = self.W_k(k)
+        V = self.W_v(v)
+        d_k = K.size(-1)
+        # from the paper: 3.2.1 *Equation 1)
+        root_dk = torch.sqrt(torch.tensor(d_k, dtype=torch.float32))
+        scores = torch.matmul(Q, K.transpose(-2, -1)) / root_dk
+        attn_weights = F.softmax(scores, dim=-1)
+        result = torch.matmul(attn_weights, V)
         ########################################################################
 
         return result
@@ -143,6 +199,9 @@ class MultiHeadAttention(nn.Module):
 
         ########################################################################
         # TODO: Add necessary member variables.
+        self.num_heads = num_heads
+        self.embed_dim = embed_dim
+        self.W_O = nn.Linear(embed_dim*num_heads, embed_dim)
         ########################################################################
 
     def forward(self, q, k, v):
@@ -152,6 +211,25 @@ class MultiHeadAttention(nn.Module):
         # TODO: Implement multi-head attention as described in Section 3.2.2
         # of the paper. Don't implement a batched version but re-use the
         # Attention class that you implemented before.
+        """
+        My Comment: 
+
+        The paper describes two main components:
+            1. concatenation of multiple attention heads
+            2. linear projection of concatenated output
+        """
+        batch_size = q.size(0)
+        # in this task, dq=dk=dv
+        input_dim = q.size(-1)
+        heads = []
+        attention = Attention(input_dim=input_dim, embed_dim=self.embed_dim)
+        for _ in range(self.num_heads):
+            heads.append(attention(q, k, v))
+        # concatenation
+        result = torch.cat(heads, dim=-1)
+        # linear projection
+        result = self.W_O(result)
+        result = result.view(batch_size, -1, self.embed_dim)
         ########################################################################
 
         return result
